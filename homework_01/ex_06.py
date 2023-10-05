b4462536cd5f68c9f81f5405de4f477cdebe76d2
tlmnhut <tlmnhut2207@gmail.com>
2023-10-05 17:35:39 +0200

    rename

---
+++ homework_01/ex_06.py
@@ -0,0 +1,11 @@
+# Hackerrank exercise
+a = int(input())
+b = int(input())
+print(a // b)
+print(a / b)
+
+# practicepython exercise
+# the int() in:
+# age = int(input("How old are you: "))
+# convert the numbers in string data type to int data type
+# every input data from input() are in string, even numbers 123 are recorded in string data type
