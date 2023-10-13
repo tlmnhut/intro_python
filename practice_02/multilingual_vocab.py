7c509fe61bd430d086c765be6a98f0cfd380b6ec
tlmnhut <tlmnhut2207@gmail.com>
2023-10-13 11:46:27 +0200

    practice 2

---
+++ practice_02/multilingual_vocab.py
@@ -0,0 +1,25 @@
+
+if __name__ == '__main__':
+    # 1. Go to https://github.com/oprogramador/most-common-words-by-language/tree/master/src/resources
+    # and download a few vocab files.
+
+    # 2. Read a vocab file, count the number of characters in each word
+    # Write the results in a new file with the format:
+    #       word_1 length_of_word_1
+    #       word_2 length_of_word_2
+    #       ... ...
+    # You should write a function to do that
+
+    # 3. Use the function above to do the same thing for other vocab files
+    # Then, you can just open the result files to see what word is longest for each language
+
+    # 4. Modify the function above to return the average number of characters in a word for each language
+    # Then you can see among the languages you chose, which language has more characters per word
+
+    # 5. Use argparse to run the script from the terminal
+
+    # 6. Extra: imagine you have 100 vocab lists in your folder, how can you read all of them without
+    # manually input each file name?
+
+    pass
+
---
+++ practice_02/wordle.py
@@ -0,0 +1,21 @@
+
+if __name__ == '__main__':
+    # Solve the Wordle game at https://wordlegame.org/
+    # It has multiple languages. You can adjust the word length as well.
+    # You may try to play a few times, then you can see that:
+    #   gray character: the word does not contain those
+    #   yellow character: the word contains this character, but in another position
+    #   green character: the word contains this character in the correct position
+
+    # 1. Write a function to help you solve the wordle
+    # You can freely design your function as you wish
+    # Hint: the previous exercise can help you choose a set of words with a specific number of characters
+    # You can read the results of the previous exercise to help you select words
+
+    # I tried and it seems quite tricky for me.
+    # Moreover, the vocab lists are limited, there are a lot of words
+    # missing in the vocab lists. You may find other better vocab lists on the web.
+
+    # 2. When you finish it, use argparse lib to run the script from the terminal.
+
+    pass
